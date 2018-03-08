c293bdc033dc4b0a85b759d9738b7f31f4b198e5
Ali Suleiman <allysuley@gmail.com>
2018-03-08 20:09:16 +0300

    Add animations to RecyclerView

--- app/src/main/java/tz/ac/udom/udomsrlite/activities/SessionsListActivity.java
+++ app/src/main/java/tz/ac/udom/udomsrlite/activities/SessionsListActivity.java
@@ -8,17 +8,21 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LayoutAnimationController;
 
 import java.text.SimpleDateFormat;
 import java.util.Locale;
 
 import tz.ac.udom.udomsrlite.R;
 import tz.ac.udom.udomsrlite.adapters.SessionListAdapter;
+import tz.ac.udom.udomsrlite.utils.TopBottomSpaceDecoration;
 
 public class SessionsListActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
     private CoordinatorLayout coordinatorLayout;
+    private LayoutAnimationController animationController;
 
     private static final String TAG = SessionsListActivity.class.getSimpleName();
 
@@ -45,9 +49,20 @@ public class SessionsListActivity extends AppCompatActivity {
         // manager
         recyclerView.setLayoutManager(new LinearLayoutManager(SessionsListActivity.this));
 
+        // decoration
+        recyclerView.addItemDecoration(new TopBottomSpaceDecoration(SessionsListActivity.this));
+
+
+        // animation
+        animationController = AnimationUtils.loadLayoutAnimation(
+                SessionsListActivity.this, R.anim.layout_animation_fall_down);
+        // animationController.setDelay(200);
+        recyclerView.setLayoutAnimation(animationController);
+
         // adapter
         SessionListAdapter adapter = new SessionListAdapter(SessionsListActivity.this);
         recyclerView.setAdapter(adapter);
+        recyclerView.scheduleLayoutAnimation();
 
         // TODO: 3/8/18 fetch sessions from db and to list
 
